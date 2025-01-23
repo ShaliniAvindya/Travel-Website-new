82edd2f729f9cb33cd6bb6f60495b848307d2e2b
Vihanga Muthumala <149398329+vihanga02@users.noreply.github.com>
2025-01-23 22:08:09 +0530

    Add Maldives Tour Packages section to HomeScreen; include descriptive text and image gallery

--- Client/src/screens/HomeScreen.js
+++ Client/src/screens/HomeScreen.js
@@ -154,6 +154,43 @@ const HomeScreen = () => {
       </Container>
 
       <HomeExperience /><br />
+      <Box
+        bgcolor="#ffffff"
+        padding="30px"
+        mt="30px"
+        sx={{
+          width: '90%',
+          position: 'relative',
+          left: '80px' ,
+          boxSizing: 'border-box',
+        }}
+      >
+        <Typography
+          variant="h3"
+          component="div"
+          sx={{
+            color: '#0A369D',
+            marginBottom: '30px',
+            fontFamily: 'Dancing Script',
+            textAlign: 'center',
+          }}
+        >
+         Maldives Tour Packages
+        </Typography>
+        <Typography
+          variant="body1"
+          color="#4472CA"
+          marginBottom={3}
+          align="justify"
+          fontFamily="Domine"
+          fontSize={17}
+        >
+          Our tour packages offer a seamless blend of luxury, adventure, and tranquility, making it the ultimate tropical escape. These packages typically include stays in overwater villas or beachfront resorts, where you can wake up to panoramic ocean views and enjoy private access to crystal-clear lagoons. Whether you're snorkeling amidst vibrant coral reefs, indulging in rejuvenating spa treatments, or savoring gourmet meals at underwater restaurants, every moment is designed for unforgettable memories. With options like sunset cruises, island-hopping excursions, and water sports, Maldives tour packages cater to honeymooners, families, and solo travelers alike, ensuring a stress-free journey in paradise.
+        </Typography>
+        <br />
+        <ImageGallery />
+      </Box>
+      
       <br /><br />
 
       <OffersSection />
